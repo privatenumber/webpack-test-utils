050f93f42aa674a270059627a2d51688c9034f41
Hiroki Osame <hiroki.osame@gmail.com>
2022-12-03 23:15:34 +0900

    wip

--- src/webpack-types.ts
+++ src/webpack-types.ts
@@ -29,12 +29,12 @@ export type BaseWebpack<
 	options: Configuration,
 ) => {
 	run: (
-		handler: (error: any, stats: Stats) => void,
+		handler: (error?: Error, stats?: Stats) => void,
 	) => void;
 
 	watch: (
 		watchOptions: any,
 		// Would like to chang error: any to error: Error but it breaks the tests
-		handler: (error: any, stats: Stats) => void,
+		handler: (error?: any, stats?: Stats) => void,
 	) => BaseWatching;
 };
